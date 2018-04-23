9d9fa8ec93a57205ca13e626772139b1ab3e43cf
Piotr Dziwinski <piotrdz@gmail.com>
2018-04-23 22:01:28 +1200

    Convert template and CSS styles

--- src/app/ng2-slider/ng2-slider.component.ts
+++ src/app/ng2-slider/ng2-slider.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ng2-slider',
   templateUrl: './ng2-slider.component.html',
-  styleUrls: ['./ng2-slider.component.css']
+  styleUrls: ['./ng2-slider.component.scss']
 })
 export class Ng2SliderComponent implements OnInit {
+  barStyle: string = '';
+  minPointerStyle: string = '';
+  maxPointerStyle: string = '';
+  ticks: any[] = [];
 
   constructor() { }
 
