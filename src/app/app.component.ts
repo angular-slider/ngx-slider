917caa47386eb9c0b582991dd3c4bc315ab93f96
Piotr Dziwinski <piotrdz@gmail.com>
2018-04-29 20:51:23 +1200

    More examples with limits applied to slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,4 +46,51 @@ export class AppComponent {
       showTicks: true
     }
   };
+
+  minMaxLimitSlider: SimpleSliderModel = {
+    value: 50,
+    options: {
+      floor: 0,
+      ceil: 100,
+      step: 1,
+      minLimit: 10,
+      maxLimit: 90
+    }
+  };
+
+  minMaxRangeSlider: RangeSliderModel = {
+    minValue: 10,
+    maxValue: 90,
+    options: {
+      floor: 0,
+      ceil: 100,
+      step: 1,
+      minRange: 10,
+      maxRange: 50
+    }
+  };
+
+  noSwitchingRangeSlider: RangeSliderModel = {
+    minValue: 10,
+    maxValue: 90,
+    options: {
+      floor: 0,
+      ceil: 100,
+      step: 1,
+      noSwitching: true
+    }
+  };
+
+  minMaxAndPushRangeSlider: RangeSliderModel = {
+    minValue: 60,
+    maxValue: 70,
+    options: {
+      floor: 0,
+      ceil: 100,
+      step: 1,
+      minRange: 10,
+      maxRange: 30,
+      pushRange: true
+    }
+  };
 }
