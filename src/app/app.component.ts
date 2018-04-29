3d6a80da7adf106763f844126824736f3696465c
Piotr Dziwinski <piotrdz@gmail.com>
2018-04-29 21:51:28 +1200

    Even fancier examples of sliders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -93,4 +93,180 @@ export class AppComponent {
       pushRange: true
     }
   };
+
+  selectionBarSlider: SimpleSliderModel = {
+    value: 10,
+    options: {
+      showSelectionBar: true
+    }
+  };
+
+  selectionBarAtEndSlider: SimpleSliderModel = {
+    value: 5,
+    options: {
+      floor: 0,
+      ceil: 10,
+      showSelectionBarEnd: true
+    }
+  };
+
+  selectionBarFromValueSlider: SimpleSliderModel = {
+    value: 5,
+    options: {
+      floor: -10,
+      ceil: 10,
+      showSelectionBarFromValue: 0
+    }
+  };
+
+  selectionBarWithGradientSlider: RangeSliderModel = {
+    minValue: 0,
+    maxValue: 80,
+    options: {
+      ceil: 100,
+      showSelectionBar: true,
+      selectionBarGradient: {
+        from: 'white',
+        to: '#FC0'
+      }
+    }
+  };
+
+  selectionBarWithDynamicColorSlider: SimpleSliderModel = {
+    value: 12,
+    options: {
+      showSelectionBar: true,
+      getSelectionBarColor: (value: number) => {
+        if (value <= 3) {
+            return 'red';
+        }
+        if (value <= 6) {
+            return 'orange';
+        }
+        if (value <= 9) {
+            return 'yellow';
+        }
+        return '#2AE02A';
+      }
+    }
+  };
+
+  dynamicPointerColorSlider: SimpleSliderModel = {
+    value: 12,
+    options: {
+        showSelectionBar: true,
+        getPointerColor: (value: number) => {
+            if (value <= 3) {
+                return 'red';
+            }
+            if (value <= 6) {
+                return 'orange';
+            }
+            if (value <= 9) {
+                return 'yellow';
+            }
+            return '#2AE02A';
+        }
+    }
+  };
+
+  floorCeilStepSlider: SimpleSliderModel = {
+    value: 12,
+    options: {
+      floor: 10,
+      ceil: 100,
+      step: 5
+    }
+  };
+
+  rtlSlider: SimpleSliderModel = {
+    value: 20,
+    options: {
+      floor: 10,
+      ceil: 100,
+      step: 5,
+      rightToLeft: true
+    }
+  };
+
+  floatSlider: SimpleSliderModel = {
+    value: 0.5,
+    options: {
+      floor: 0,
+      ceil: 2,
+      step: 0.1,
+      precision: 1
+    }
+  };
+
+  customDisplayFunctionSlider: RangeSliderModel = {
+    minValue: 100,
+    maxValue: 400,
+    options: {
+      floor: 0,
+      ceil: 500,
+      translate: (value: number) => {
+        return '$' + value;
+      }
+    }
+  };
+
+  customDisplayFunctionHtmlSlider: RangeSliderModel = {
+    minValue: 100,
+    maxValue: 400,
+    options: {
+      floor: 0,
+      ceil: 500,
+      translate: (value: number, sliderId: any, label: any) => {
+        switch (label) {
+          case 'model':
+            return '<b>Min price:</b> $' + value;
+          case 'high':
+            return '<b>Max price:</b> $' + value;
+          default:
+            return '$' + value;
+        }
+      }
+    }
+  };
+
+  alphabetSlider: SimpleSliderModel = {
+    value: this.letterToIndex('E'),
+    options: {
+      stepsArray: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) => {
+        return { value: this.letterToIndex(letter) };
+      }),
+      translate: (value: number, sliderId: any, label: any) => {
+        return this.indexToLetter(value);
+      }
+    }
+  };
+
+  dateSlider: SimpleSliderModel = {
+    value: this.createDateRange()[0].getTime(),
+    options: {
+      stepsArray: this.createDateRange().map((date: Date) => {
+        return { value: date.getTime() };
+      }),
+      translate: (value: number, sliderId: any, label: any) => {
+        return new Date(value).toDateString();
+      }
+    }
+  };
+
+  createDateRange(): Date[] {
+    const dates: Date[] = [];
+    for (let i = 1; i <= 31; i++) {
+      dates.push(new Date(2016, 7, i));
+    }
+    return dates;
+  }
+
+  indexToLetter(index: number): string {
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[index];
+  }
+
+  letterToIndex(letter: string): number {
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(letter);
+  }
 }
