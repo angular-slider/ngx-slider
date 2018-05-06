4d3535238cf8cc8c6050be32665e8bdb275e2830
Piotr Dziwinski <piotrdz@gmail.com>
2018-05-06 19:21:35 +1200

    Update demo app header and fix e2e spec failure

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-slider App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Angular 5 slider demo!');
   });
 });
