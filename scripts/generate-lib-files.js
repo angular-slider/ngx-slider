3ce3ef4db421dab59f7d3b7630d8fc6407d1e2b9
Piotr Dziwinski <piotrdz@gmail.com>
2018-05-12 17:43:52 +1200

    Copy main README.md for NPM package

--- scripts/generate-lib-files.js
+++ scripts/generate-lib-files.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+// Generate package.json file based on package.json.template and main package.json
+
 const mainFile = path.resolve(__dirname, '../package.json');
-const libTemplateFile = path.resolve(__dirname, '../src/ng5-slider/package.json.template')
-const libFile = path.resolve(__dirname, '../src/ng5-slider/package.json')
+const libTemplateFile = path.resolve(__dirname, '../src/ng5-slider/package.json.template');
+const libFile = path.resolve(__dirname, '../src/ng5-slider/package.json');
 
 const libTemplateConfig = JSON.parse(fs.readFileSync(libTemplateFile, { encoding: 'utf8' }));
 const mainConfig = JSON.parse(fs.readFileSync(mainFile, { encoding: 'utf8' }));
@@ -20,3 +22,11 @@ delete libConfig.keysToCopyFromMainPackageJson;
 const prettyPrintedLibConfig = JSON.stringify(libConfig, null, 2);
 
 fs.writeFileSync(libFile, prettyPrintedLibConfig, { encoding: 'utf8' });
+
+// Copy main README.md file
+
+const mainReadmeFile = path.resolve(__dirname, '../README.md');
+const libReadmeFile = path.resolve(__dirname, '../src/ng5-slider/README.md');
+
+const mainReadme = fs.readFileSync(mainReadmeFile, { encoding: 'utf8'});
+fs.writeFileSync(libReadmeFile, mainReadme, {encoding: 'utf8'});
