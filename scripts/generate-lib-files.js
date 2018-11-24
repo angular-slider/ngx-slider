0277cc098d671b018e698fb868771c9e7837856e
Piotr Dziwinski <piotrdz@gmail.com>
2018-11-25 10:28:01 +1300

    Generate public_api.ts based on config file

--- scripts/generate-lib-files.js
+++ scripts/generate-lib-files.js
@@ -1,46 +1,74 @@
 const fs = require('fs');
 const path = require('path');
 
-// Generate package.json file based on package.json.template and main package.json
+/** Generate package.json file based on package.json.template and main package.json */
+function generatePackageJson() {
+  const mainFile = path.resolve(__dirname, '../package.json');
+  const libTemplateFile = path.resolve(__dirname, '../src/ng5-slider/package.json.template');
+  const libFile = path.resolve(__dirname, '../src/ng5-slider/package.json');
 
-const mainFile = path.resolve(__dirname, '../package.json');
-const libTemplateFile = path.resolve(__dirname, '../src/ng5-slider/package.json.template');
-const libFile = path.resolve(__dirname, '../src/ng5-slider/package.json');
+  const libTemplateConfig = JSON.parse(fs.readFileSync(libTemplateFile, { encoding: 'utf8' }));
+  const mainConfig = JSON.parse(fs.readFileSync(mainFile, { encoding: 'utf8' }));
 
-const libTemplateConfig = JSON.parse(fs.readFileSync(libTemplateFile, { encoding: 'utf8' }));
-const mainConfig = JSON.parse(fs.readFileSync(mainFile, { encoding: 'utf8' }));
+  let libConfig = {};
 
-let libConfig = {};
+  for (let key of libTemplateConfig.keysToCopyFromMainPackageJson) {
+      libConfig[key] = mainConfig[key];
+  }
+  libConfig.dependencies = {};
+  for (let dependency of libTemplateConfig.dependenciesToCopyFromMainPackageJson) {
+    libConfig.dependencies[dependency] = mainConfig.dependencies[dependency];
+  }
+  libConfig.peerDependencies = {};
+  for (let dependency of libTemplateConfig.dependenciesToCopyAsPeerDependenciesFromMainPackageJson) {
+    libConfig.peerDependencies[dependency] = mainConfig.dependencies[dependency];
+  }
 
-for (let key of libTemplateConfig.keysToCopyFromMainPackageJson) {
-    libConfig[key] = mainConfig[key];
+  libConfig = Object.assign({}, libConfig, libTemplateConfig, libConfig);
+  delete libConfig.keysToCopyFromMainPackageJson;
+  delete libConfig.dependenciesToCopyFromMainPackageJson;
+  delete libConfig.dependenciesToCopyAsPeerDependenciesFromMainPackageJson;
+
+  for (let dependency of libTemplateConfig.dependenciesToCopyFromMainPackageJson) {
+    libConfig.ngPackage.whitelistedNonPeerDependencies.push(dependency);
+  }
+
+  const prettyPrintedLibConfig = JSON.stringify(libConfig, null, 2);
+
+  fs.writeFileSync(libFile, prettyPrintedLibConfig, { encoding: 'utf8' });
 }
-libConfig.dependencies = {};
-for (let dependency of libTemplateConfig.dependenciesToCopyFromMainPackageJson) {
-  libConfig.dependencies[dependency] = mainConfig.dependencies[dependency];
-}
-libConfig.peerDependencies = {};
-for (let dependency of libTemplateConfig.dependenciesToCopyAsPeerDependenciesFromMainPackageJson) {
-  libConfig.peerDependencies[dependency] = mainConfig.dependencies[dependency];
-}
 
-libConfig = Object.assign({}, libConfig, libTemplateConfig, libConfig);
-delete libConfig.keysToCopyFromMainPackageJson;
-delete libConfig.dependenciesToCopyFromMainPackageJson;
-delete libConfig.dependenciesToCopyAsPeerDependenciesFromMainPackageJson;
+/** Copy main README.md file */
+function copyReadmeMd() {
+  const mainReadmeFile = path.resolve(__dirname, '../README.md');
+  const libReadmeFile = path.resolve(__dirname, '../src/ng5-slider/README.md');
 
-for (let dependency of libTemplateConfig.dependenciesToCopyFromMainPackageJson) {
-  libConfig.ngPackage.whitelistedNonPeerDependencies.push(dependency);
+  const mainReadme = fs.readFileSync(mainReadmeFile, { encoding: 'utf8'});
+  fs.writeFileSync(libReadmeFile, mainReadme, {encoding: 'utf8'});
 }
 
-const prettyPrintedLibConfig = JSON.stringify(libConfig, null, 2);
+/** Convert public_api.json to public_api.ts */
+function generatePublicApiTs() {
+  const configFile = path.resolve(__dirname, '../src/ng5-slider/lib/public_api.json');
+  const tsFile = path.resolve(__dirname, '../src/ng5-slider/lib/public_api.ts');
 
-fs.writeFileSync(libFile, prettyPrintedLibConfig, { encoding: 'utf8' });
+  const config = JSON.parse(fs.readFileSync(configFile, { encoding: 'utf8' }));
 
-// Copy main README.md file
+  let tsFileContent = '';
 
-const mainReadmeFile = path.resolve(__dirname, '../README.md');
-const libReadmeFile = path.resolve(__dirname, '../src/ng5-slider/README.md');
+  for (let exportDef of config.exports) {
+    if (exportDef.what instanceof Array) {
+      const whats = exportDef.what.join(', ');
+      tsFileContent += `export { ${whats} } from '${exportDef.file}';\n`;
+    } else {
+      tsFileContent += `export ${exportDef.what} from '${exportDef.file}';\n`;
+    }
+  }
 
-const mainReadme = fs.readFileSync(mainReadmeFile, { encoding: 'utf8'});
-fs.writeFileSync(libReadmeFile, mainReadme, {encoding: 'utf8'});
+  fs.writeFileSync(tsFile, tsFileContent, {encoding: 'utf8'});
+}
+
+
+generatePackageJson();
+generatePublicApiTs();
+copyReadmeMd();
