e54d5af935e23a733c54b991826f60a75530270d
Darwin Gualotuña <daosgc@gmail.com>
2022-01-03 18:02:56 -0500

    Expose getStepLegend to show a custom object when we have stepsArray

--- src/ngx-slider/lib/slider.component.ts
+++ src/ngx-slider/lib/slider.component.ts
@@ -1156,7 +1156,9 @@ export class SliderComponent implements OnInit, AfterViewInit, OnChanges, OnDest
       let legend: string = null;
       if (!ValueHelper.isNullOrUndefined(this.viewOptions.stepsArray)) {
         const step: CustomStepDefinition = this.viewOptions.stepsArray[value];
-        if (!ValueHelper.isNullOrUndefined(step)) {
+        if (!ValueHelper.isNullOrUndefined(this.viewOptions.getStepLegend)) {
+          legend = this.viewOptions.getStepLegend(step);
+        } else if (!ValueHelper.isNullOrUndefined(step)) {
           legend = step.legend;
         }
       } else if (!ValueHelper.isNullOrUndefined(this.viewOptions.getLegend)) {
