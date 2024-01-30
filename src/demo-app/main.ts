c50716b0017305e92df4c38b132c48e55ac887b0
Dani Santos <daniramossantos9@gmail.com>
2024-01-30 23:53:40 +0000

    updated tsconfig and angular json files

--- src/demo-app/main.ts
+++ src/demo-app/main.ts
@@ -8,5 +8,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err: any): void => console.log(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.log(err));
