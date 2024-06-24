013e1486115abf02324787ae862ec20b2d8d673e
Piotr Dziwinski <piotrdz@gmail.com>
2024-06-24 19:23:33 +1200

    Upgrade to Angular 18 (issue #379)

--- src/ngx-slider/lib/slider.component.spec.ts
+++ src/ngx-slider/lib/slider.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SliderComponent } from './slider.component';
 import { SliderElementDirective } from './slider-element.directive';
@@ -10,7 +10,7 @@ describe('SliderComponent', () => {
   let component: SliderComponent;
   let fixture: ComponentFixture<SliderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         SliderComponent,
